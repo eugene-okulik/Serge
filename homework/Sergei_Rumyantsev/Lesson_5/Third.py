cee0c1eed02aee1c57e2ce606fe51dde99224af0
Kramsh <sergeroq@gmail.com>
2025-08-30 21:56:21 +0300

    Fix Lint_V2

--- homework/Sergei_Rumyantsev/Lesson_5/Third.py
+++ homework/Sergei_Rumyantsev/Lesson_5/Third.py
@@ -1,7 +1,7 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
-a, b,c = students
+a, b, c = students
 d, e, f = subjects
 
 print(f'Students {a}, {b}, {c} study these subjects: {d}, {e}, {f}')
