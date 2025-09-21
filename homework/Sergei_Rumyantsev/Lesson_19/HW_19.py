7ca9d28aa5f1b1d6ecfa4aadae33e874ee7e6e39
Kramsh <sergeroq@gmail.com>
2025-09-21 08:22:09 +0300

    HW_19_v2

--- homework/Sergei_Rumyantsev/Lesson_19/HW_19.py
+++ homework/Sergei_Rumyantsev/Lesson_19/HW_19.py
@@ -4,17 +4,17 @@ import json
 
 def create():
     body = json.dumps({
-    "name": "Test object",
-    "data": {
-        "color": "Green",
-        "size": "Medium"
-    }
+        "name": "Test object",
+        "data": {
+            "color": "Green",
+            "size": "Medium",
+        },
     })
     headers = {'Content-type': 'application/json'}
-    response = requests.post(''
+    response = requests.post(
         'http://objapi.course.qa-practice.com/object',
         data=body,
-        headers=headers
+        headers=headers,
     ).json()
     return response
 
@@ -23,27 +23,25 @@ post_data = create()
 post_id = 0
 for key, value in post_data.items():
     if key == 'id':
-        post_id = post_data[key]
+        post_id = value
 
 
 def change_data_put():
     body = json.dumps({
-    "name": "Test object",
-    "data": {
-        "color": "Green",
-        "size": "Large"
-    }
+        "name": "Test object",
+        "data": {
+            "color": "Green",
+            "size": "Large",
+        },
     })
     headers = {'Content-type': 'application/json'}
-    response = requests.put(''
+    response = requests.put(
         f'http://objapi.course.qa-practice.com/object/{post_id}',
         data=body,
-        headers=headers
+        headers=headers,
     ).json()
     return response
 
-
-change_data_put()
 
 put_data = change_data_put()
 put_size = ''
@@ -51,21 +49,21 @@ for key, value in put_data.items():
     if key == 'data':
         for data_key, data_value in value.items():
             if data_key == 'size':
-                put_size = value[data_key]
+                put_size = data_value
 
 
 def change_data_patch():
     body = json.dumps({
-    "data": {
-        "color": "Red",
-        "size": put_size
-    }
+        "data": {
+            "color": "Red",
+            "size": put_size,
+        },
     })
     headers = {'Content-type': 'application/json'}
-    response = requests.patch(''
+    response = requests.patch(
         f'http://objapi.course.qa-practice.com/object/{post_id}',
         data=body,
-        headers=headers
+        headers=headers,
     ).json()
     print(response)
 
@@ -74,7 +72,7 @@ change_data_patch()
 
 
 def delete_data():
-    response = requests.delete(f'http://objapi.course.qa-practice.com/object/{post_id}')
+    requests.delete(f'http://objapi.course.qa-practice.com/object/{post_id}')
 
 
 delete_data()
